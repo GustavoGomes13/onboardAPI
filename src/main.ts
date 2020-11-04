744f6a623deab0c20790a9ef72f3a8c8cbbeff3b
Gustavo Gomes <gustavoalang13@gmail.com>
2020-11-03 22:40:05 -0300

    Front

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express/interfaces/nest-express-application.interface';
+//import * as hbs from 'hbs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
@@ -11,6 +12,7 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+  //hbs.registerPartials(join(__dirname, '..', 'views/partials'));
 
   await app.listen(3000);
 
